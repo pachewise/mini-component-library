f4a8adb6a47bc9932a2a40bd7fb0a484600741da
José Pacheco <pachewise@gmail.com>
2021-03-25 22:08:30 -0700

    ProgressBar (hacky Border-radius)

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,68 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLE = {
+  large: {
+    padding: 2,
+    height: 15,
+  },
+  medium: {
+    padding: 0,
+    height: 10,
+  },
+  small: {
+    padding: 0,
+    height: 5,
+  },
+};
+
+const width = 100;
+const maxBorderRadius = 4;
+const Wrapper = styled.div`
+  border-radius: ${maxBorderRadius}px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  height: var(--height);
+  width: ${width}px;
+`;
+const Progress = styled.div`
+  height: 100%;
+  width: var(--width);
+  background-color: ${COLORS.primary};
+  border-radius: ${maxBorderRadius}px var(--border-radius) var(--border-radius)
+    ${maxBorderRadius}px;
+`;
+
+const calcRadius = (value) =>
+  Math.min(
+    maxBorderRadius,
+    Math.max((value - width * 0.98) / (width * 0.02), 0)
+  );
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const style = STYLE[size];
+  const borderRadius = `${calcRadius(value)}px`;
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--height": `${style.height}px`,
+        "--padding": `${style.padding}px`,
+      }}
+    >
+      <Progress
+        style={{ "--width": `${value}%`, "--border-radius": borderRadius }}
+      >
+        <VisuallyHidden>Progress: {value}%</VisuallyHidden>
+      </Progress>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
